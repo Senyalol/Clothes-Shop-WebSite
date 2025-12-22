4c44a471051e21ea9db2063781c8ae3bac7986f3
Simon <s.a.naumenko@students.psu.by>
2025-12-22 20:56:07 +0300

    add new endpoint to permitAll

--- CShop/src/main/java/com/ClotheShop/CShop/Security/SecurityConfig.java
+++ CShop/src/main/java/com/ClotheShop/CShop/Security/SecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
